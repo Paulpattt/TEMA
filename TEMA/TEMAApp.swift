c9adaa4ec00b68d1803ed13d1a9c5df4add398f9
Paul PATUREL <paturel.etincelle@icloud.com>
2025-01-26 23:00:54 +0100

    Initial Commit

---
+++ TEMA/TEMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEMAApp.swift
+//  TEMA
+//
+//  Created by Paul Paturel on 26/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TEMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TEMATests/TEMATests.swift
@@ -0,0 +1,17 @@
+//
+//  TEMATests.swift
+//  TEMATests
+//
+//  Created by Paul Paturel on 26/01/2025.
+//
+
+import Testing
+@testable import TEMA
+
+struct TEMATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
